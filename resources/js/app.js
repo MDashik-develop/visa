ce5f3f1cc8bdeaf6403d6427ada41bda1848a78b
MDashik-develop <MDashik.design.laptop@gmail.com>
2025-04-17 02:46:03 +0600

    Remove commented-out Swiper code from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,25 +10,6 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
-// swiper animation custom
-// document.addEventListener("DOMContentLoaded", function () {
-//     const swiper = new Swiper(".mySwiper", {
-//         loop: true,
-//         autoplay: {
-//             delay: 3000,
-//         },
-//         pagination: {
-//             el: ".swiper-pagination",
-//             clickable: true,
-//         },
-//         navigation: {
-//             nextEl: ".swiper-button-next",
-//             prevEl: ".swiper-button-prev",
-//         },
-//     });
-// });
 
 // slick carousel
     window.$ = $;
