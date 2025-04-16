3bee98c8ea0501c42019f8f3d370baff403f1443
MDashik-develop <MDashik.design.laptop@gmail.com>
2025-04-17 00:51:44 +0600

    Refactor header and navigation structure for improved layout and responsiveness

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -16,10 +16,10 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+            {{-- @include('layouts.navigation') --}}
 
             <!-- Page Heading -->
-            {{-- @include('layouts.frontend.header') --}}
+            @include('layouts.frontend.header')
 
             <!-- Page Content -->
             <main>
